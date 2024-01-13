7d9388ba5e674a13044e0b67faf6f16ae58d47ec
Caroline Vieira <carolinesanvieira@gmail.com>
2024-01-13 20:47:26 +0100

    feat: add nativebase

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
 import { Text, View, StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
+    <NativeBaseProvider>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
        />
-     {fontsLoaded ? <Text>Hello Peach!</Text> : <View />}
+     {fontsLoaded ? <View /> : <View />}
     </View>
+    </NativeBaseProvider>
   );
 }
