96dcce544a26aa1010f8b250d81cd31e3f0337b9
Caroline Vieira <carolinesanvieira@gmail.com>
2024-01-15 18:52:42 +0100

    feat: customize default theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import { View, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from '@/theme';
 import { Loading } from '@/components/Loading';
 
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
        />
-     <Loading />
+    { fontsLoaded ? <View /> : <Loading />}
     </View>
     </NativeBaseProvider>
   );
