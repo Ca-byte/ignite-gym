78d8f8a58a28a5de4c57eb9cd0c599e0365503a5
Caroline Vieira <carolinesanvieira@gmail.com>
2024-02-07 21:55:22 +0100

    feat: user photo component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,15 @@
 import { HStack, Heading, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader(){
 	return(
 		<HStack bg="gray.600" pt={16} pb={5} alignItems="center">
+			<UserPhoto 
+			source={{ uri: 'https://github.com/Ca-byte.png'}}
+			alt="User Image Profile"
+			size={16}
+			mr={4}
+			/>
 			<VStack>
 			<Text color="gray.100" fontSize="md">
 				 Hi
