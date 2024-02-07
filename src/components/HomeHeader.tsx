ed6fcffc6769c59868b6fa2ffda0f8209f08f74d
Caroline Vieira <carolinesanvieira@gmail.com>
2024-02-07 21:44:14 +0100

    feat: style home header componennt

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,10 +2,12 @@ import { HStack, Heading, Text, VStack } from "native-base";
 
 export function HomeHeader(){
 	return(
-		<HStack>
+		<HStack bg="gray.600" pt={16} pb={5} alignItems="center">
 			<VStack>
-			<Text color="gray.100"> Hi</Text>
-			<Heading color="gray.100">
+			<Text color="gray.100" fontSize="md">
+				 Hi
+			</Text>
+			<Heading color="gray.100" fontSize="md">
 				Caroline
 			</Heading>
 			</VStack>
