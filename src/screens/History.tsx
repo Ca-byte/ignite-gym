f07fa305990bf4cb1d03063909fa50849c412b54
Caroline Vieira <carolinesanvieira@gmail.com>
2024-02-19 18:33:19 +0100

    feat: empty list message

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { SectionList } from "react-native";
 import { HistoryCard } from "@/components/HistoryCard";
 import { ScreenHeader } from "@/components/ScreenHeader";
-import { Heading, VStack} from "native-base";
+import { Heading, Text, VStack} from "native-base";
 
 export function History(){
 	const [exercises, setExercises] = useState([
@@ -28,6 +28,13 @@ export function History(){
           <Heading color="gray.200" fontSize="md" mt={10} mb={3} px={8}>
             {section.title}
           </Heading>
+        )}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            There are no exercises registered yet. {'\n'}
+            Shall we exercise today?
+          </Text>
         )}
         
       />
