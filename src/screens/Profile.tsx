f5970229ba10830d2e5bbc0826c607de31b55d9c
Caroline Vieira <carolinesanvieira@gmail.com>
2024-02-22 21:57:22 +0100

    feat: add phot profile skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,31 @@
 import { ScreenHeader } from "@/components/ScreenHeader";
 import { UserPhoto } from "@/components/UserPhoto";
-import { Center, ScrollView, Text, VStack } from "native-base";
+import { Center, ScrollView, VStack, Skeleton } from "native-base";
+import { useState } from "react";
+
+const PHOTO_SIZE = 33;
 
 export function Profile(){
+	const [isPhotoLoaded, setIsPhotoLoaded]= useState(true)
 	return (
 		<VStack>
 			<ScreenHeader title="Profile" />
 			<ScrollView>
 				<Center mt={6} px={10}>
-				<UserPhoto 
-					source={{ uri: 'https://github.com/Ca-byte.png'}}
-					alt="User Image Profile"
-					size={33}
-				/>
+					{ isPhotoLoaded ?
+						<Skeleton 
+							w={PHOTO_SIZE} 
+							h={PHOTO_SIZE} 
+							rounded="full" 
+							startColor="gray.400"
+							endColor="green.500"
+						/> :
+						<UserPhoto 
+							source={{ uri: 'https://github.com/Ca-byte.png'}}
+							alt="User Image Profile"
+							size={PHOTO_SIZE}
+						/>
+					}
 				</Center>
 			</ScrollView>
 		</VStack>
