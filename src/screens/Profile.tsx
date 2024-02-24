249cb9e91d103ce8ab63ef330447050f643179af
Caroline Vieira <carolinesanvieira@gmail.com>
2024-02-24 21:50:15 +0100

    feat: add change password input

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { ScreenHeader } from "@/components/ScreenHeader";
 import { UserPhoto } from "@/components/UserPhoto";
-import { Center, ScrollView, VStack, Skeleton, Text } from "native-base";
+import { Center, ScrollView, VStack, Skeleton, Text, Heading} from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 
@@ -43,6 +44,31 @@ export function Profile(){
 						isDisabled
 					/>
 				</Center>
+				<VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+						Change Password
+          </Heading>
+
+          <Input 
+            bg="gray.600"
+            placeholder="Old password"
+            secureTextEntry
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="New password"
+            secureTextEntry
+          />
+
+          <Input 
+            bg="gray.600"
+            placeholder="Confirm new password"
+            secureTextEntry
+          />
+
+          <Button title="Update" mt={4} />
+        </VStack>
 			</ScrollView>
 		</VStack>
 	)
