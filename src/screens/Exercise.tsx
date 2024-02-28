b6b38d2cfe72d666e7343381aefa5ea67523c336
Caroline Vieira <carolinesanvieira@gmail.com>
2024-02-28 21:01:14 +0100

    feat: exercise header

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,8 +1,10 @@
-import { Icon, VStack } from "native-base";
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
 import { TouchableOpacity } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@/routes/app.route";
+
+import BodySvg from '@assets/body.svg'
 
 export function Exercise(){
 
@@ -18,6 +20,29 @@ export function Exercise(){
 				<TouchableOpacity onPress={handleGoBack}>
 					<Icon as={Feather} name="arrow-left" color="green-500" size={6}/>
 				</TouchableOpacity>
+
+				<HStack 
+					justifyContent="space-between" 
+					mt={4} mb={8} 
+					alignItems="center"
+				>
+					<Heading 
+						color="gray.100" 
+						fontSize="lg" 
+						flexShrink={1}
+					>
+						Front Pull
+					</Heading>
+					<HStack alignItems="center">
+						<BodySvg />
+						<Text 
+							color="gray.200" 
+							ml={1} textTransform="capitalize"
+						>
+							Back
+						</Text>
+					</HStack>
+				</HStack>
 			</VStack>
 		</VStack>
 	)
