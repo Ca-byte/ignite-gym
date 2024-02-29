2f4ec1eb63b168862e889d6e6538ae62d3f10c03
Caroline Vieira <carolinesanvieira@gmail.com>
2024-02-29 21:53:26 +0100

    feat: exercise screen complete

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,10 +1,14 @@
-import { HStack, Heading, Icon, Text, VStack } from "native-base";
+import { Box, HStack, Heading, Icon, Image, Text, VStack } from "native-base";
 import { TouchableOpacity } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@/routes/app.route";
 
+import SeriesSvg from '@assets/series.svg';
+import RepetitionsSvg from '@assets/repetitions.svg';
+
 import BodySvg from '@assets/body.svg'
+import { Button } from "@/components/Button";
 
 export function Exercise(){
 
@@ -44,6 +48,38 @@ export function Exercise(){
 					</HStack>
 				</HStack>
 			</VStack>
+			<VStack p={8}>
+			<Image
+				w="full"
+				h={80}
+				source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+				alt="Exercise Image"
+				mb={3}
+				resizeMode="cover"
+				rounded="lg"
+        />
+			</VStack>
+			<Box bg="gray.600" rounded="md" pb={4} px={4}>
+          <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
+            <HStack>
+              <SeriesSvg />
+              <Text color="gray.200" ml="2">
+                3 series
+              </Text>
+            </HStack>
+
+            <HStack>
+              <RepetitionsSvg />
+              <Text color="gray.200" ml="2">
+                12 repetitions
+              </Text>
+            </HStack>
+          </HStack>
+
+          <Button 
+            title="Well Done!"
+          />
+        </Box>
 		</VStack>
 	)
 }
