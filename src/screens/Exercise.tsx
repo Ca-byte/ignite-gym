6a70b0077f41e5730cdab27adde126b2a82b0646
Caroline Vieira <carolinesanvieira@gmail.com>
2024-03-01 22:51:41 +0100

    fix: screens recap

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,13 +1,13 @@
-import { Box, HStack, Heading, Icon, Image, Text, VStack } from "native-base";
+import { Box, HStack, Heading, Icon, Image, Text, VStack, ScrollView } from "native-base";
 import { TouchableOpacity } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@/routes/app.route";
 
-import SeriesSvg from '@assets/series.svg';
-import RepetitionsSvg from '@assets/repetitions.svg';
+import SeriesSvg from '../assets/series.svg';
+import RepetitionsSvg from '../assets/repetitions.svg';
 
-import BodySvg from '@assets/body.svg'
+import BodySvg from '../assets/body.svg'
 import { Button } from "@/components/Button";
 
 export function Exercise(){
@@ -20,66 +20,70 @@ export function Exercise(){
 
 	return (
 		<VStack flex={1}>
-			<VStack px={8} bg="gray.600" pt={12}>
-				<TouchableOpacity onPress={handleGoBack}>
-					<Icon as={Feather} name="arrow-left" color="green-500" size={6}/>
-				</TouchableOpacity>
+			<ScrollView>
+				<VStack px={8} bg="gray.600" pt={12}>
+					<TouchableOpacity onPress={handleGoBack}>
+						<Icon as={Feather} name="arrow-left" color="green.500" size={6}/>
+					</TouchableOpacity>
 
-				<HStack 
-					justifyContent="space-between" 
-					mt={4} mb={8} 
-					alignItems="center"
-				>
-					<Heading 
-						color="gray.100" 
-						fontSize="lg" 
-						flexShrink={1}
+					<HStack 
+						justifyContent="space-between" 
+						mt={4} mb={8} 
+						alignItems="center"
 					>
-						Front Pull
-					</Heading>
-					<HStack alignItems="center">
-						<BodySvg />
-						<Text 
-							color="gray.200" 
-							ml={1} textTransform="capitalize"
+						<Heading 
+							color="gray.100" 
+							fontSize="lg" 
+							flexShrink={1}
 						>
-							Back
-						</Text>
+							Front Pull
+						</Heading>
+						<HStack alignItems="center">
+							<BodySvg />
+							<Text 
+								color="gray.200" 
+								ml={1} textTransform="capitalize"
+							>
+								Back
+							</Text>
+						</HStack>
 					</HStack>
-				</HStack>
-			</VStack>
-			<VStack p={8}>
-			<Image
-				w="full"
-				h={80}
-				source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
-				alt="Exercise Image"
-				mb={3}
-				resizeMode="cover"
-				rounded="lg"
-        />
-			</VStack>
-			<Box bg="gray.600" rounded="md" pb={4} px={4}>
-          <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
-            <HStack>
-              <SeriesSvg />
-              <Text color="gray.200" ml="2">
-                3 series
-              </Text>
-            </HStack>
+				</VStack>
+					<VStack p={8}>
+					<Image
+						w="full"
+						h={80}
+						source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+						alt="Exercise Image"
+						mb={3}
+						resizeMode="cover"
+						rounded="lg"
+						/>
+					</VStack>
 
-            <HStack>
-              <RepetitionsSvg />
-              <Text color="gray.200" ml="2">
-                12 repetitions
-              </Text>
-            </HStack>
-          </HStack>
+				<Box bg="gray.600" rounded="md" pb={4} px={4}>
+					<HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
+						<HStack>
+							<SeriesSvg />
+							<Text color="gray.200" ml="2">
+								3 series
+							</Text>
+						</HStack>
 
-          <Button 
-            title="Well Done!"
-          />
-        </Box>
+						<HStack>
+							<RepetitionsSvg />
+							<Text color="gray.200" ml="2">
+								12 repetitions
+							</Text>
+						</HStack>
+					</HStack>
+
+					<Button 
+						title="Well Done!"
+					/>
+				</Box>
+			</ScrollView>
 		</VStack>
+
 	)
 }
