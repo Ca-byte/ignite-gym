937c1e5b7c148260a5b75eb383b607d78f1b6622
Caroline Vieira <carolinesanvieira@gmail.com>
2024-03-19 19:40:15 +0100

    style validation message

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -63,14 +63,11 @@ export function SignUp(){
 								placeholder='Name'
 								onChangeText={onChange}
 								value={value}
+								errorMessage={errors.name?.message}
 							/>
 					)}
 					 />
-					<Text 
-						color="white">
-						{errors.name?.message}
-					</Text>
-					
+			
 					<Controller 
 						control={control}
 						name="email"
@@ -90,14 +87,11 @@ export function SignUp(){
 								autoCapitalize='none'
 								onChangeText={onChange}
 								value={value}
+								errorMessage={errors.email?.message}
 							/>
 						)}
 				 	/>
-					<Text 
-						color="white">
-						{errors.email?.message}
-					</Text>
-				
+					
 					<Controller 
 						control={control}
 						name="password"
