6b31f26ddde60a11c81240ad790aa2eb214d3f9f
Caroline Vieira <carolinesanvieira@gmail.com>
2024-04-10 22:05:40 +0200

    feat: Axios Exeptions

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -6,8 +6,10 @@ import { Input } from "@/components/Input";
 import { api } from '@/services/api';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigation } from "@react-navigation/native";
+import axios from 'axios';
 import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
 import { Controller, useForm } from 'react-hook-form';
+import { Alert } from 'react-native';
 import * as yup from 'yup';
 
 type FormDataProps = {
@@ -35,8 +37,14 @@ export function SignUp(){
 	}
 
 	async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await api.post('/users', { name, email, password });
-    console.log(response.data);
+		try {
+      const response = await api.post('/users', { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      if(axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
 	}
 
 	return (
