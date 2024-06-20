3b72fe8f3c82c0d0db1e4346196cb32e645c6cf4
Caroline Vieira <carolinesanvieira@gmail.com>
2024-06-20 22:43:56 +0200

    feat: check refresh token or log out user

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,4 @@
+import { storageAuthTokenGet } from "@/storage/storageAuthToken";
 import { AppError } from "@/utils/AppError";
 import axios, { AxiosInstance } from "axios";
 
@@ -16,13 +17,17 @@ const api = axios.create({
 
 
 api.registerInterceptTokenManager = singOut => {
-  const interceptTokenManager = api.interceptors.response.use((response) => response, requestError => {
+  const interceptTokenManager = api.interceptors.response.use((response) => response, async (requestError) => {
     if(requestError.response?.status === 401) {
       if(requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
 
+        const { refresh_token } = await storageAuthTokenGet();
+        if(!refresh_token)
+          singOut();
+        return Promise.reject(requestError);
+
       }
 
-      singOut();
 
     }
 
